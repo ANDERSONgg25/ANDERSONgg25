014b587058156ed92be5cd6b743af05a4b00d5e0
RYUMAN HUB <166325158+ANDERSONgg25@users.noreply.github.com>
2025-01-05 17:20:19 -0500

    Update tp.lua

--- tp.lua
+++ tp.lua
@@ -1,5 +1,5 @@
 local player = game.Players.LocalPlayer
-local tp = Vector3.new(0, -500, 0)
+local tp = Vector3.new(0, -300, 0)
 local character = player.Character or player.CharacterAdded:Wait()
 local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
 humanoidRootPart.CFrame = CFrame.new(tp)
